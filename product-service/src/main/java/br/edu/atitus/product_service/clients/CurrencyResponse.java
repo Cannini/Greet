90e6127b7150e731b8ea14ccf906274bed781ade
Gabriel Cannini <gabrielcannini@hotmail.com>
2025-10-16 23:45:13 -0300

    done

---
+++ product-service/src/main/java/br/edu/atitus/product_service/clients/CurrencyResponse.java
@@ -0,0 +1,71 @@
+package br.edu.atitus.product_service.clients;
+
+public class CurrencyResponse {
+
+	private Long id;
+	
+	private String source;
+	
+	private String target;
+	
+	private double conversionRate;
+	
+	private double conversionValue;
+	
+	private String enviroment;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+
+	public double getConversionRate() {
+		return conversionRate;
+	}
+
+	public void setConversionRate(double conversionRate) {
+		this.conversionRate = conversionRate;
+	}
+
+	public double getConversionValue() {
+		return conversionValue;
+	}
+
+	public void setConversionValue(double conversionValue) {
+		this.conversionValue = conversionValue;
+	}
+
+	public String getEnviroment() {
+		return enviroment;
+	}
+
+	public void setEnviroment(String enviroment) {
+		this.enviroment = enviroment;
+	}
+
+	public double getConvertedValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+}
